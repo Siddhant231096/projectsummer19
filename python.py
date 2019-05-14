b18c89acb2da9aa03caa4a71b5be78b72d75de90
root <root@ip-172-31-7-255.ap-south-1.compute.internal>
2019-05-14 12:33:57 +0000

    first commit

---
+++ python.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+
+options="""
+press 1. to check ec2 instance RAM
+press 2. to check since how long ec2 instance is running
+prees 3. to check a command named tree and install it
+press 4. to chech current time and date
+press 5. to login in your facebook account and update status hello world
+press 6. to search something on google
+press 7. to list all the IP & MAc in your current wifi
+press 8. to disconnet all wifi connected device execpt YOU 
+press 9. to launch  on rhel 7.5 based free ec2 instance
+press 10. to check status of ec3 instance 
+
+
+
+"""
+
+print(options)
+print("@@@@@@@@@@@@@@@@@@@@@@@@@")
+
+choice = input()
+
